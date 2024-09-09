81411bf4404442709b8bd73948ead24b8eb68361
Hugo Loiola <hugoloiola.1@gmail.com>
2024-09-09 16:04:37 -0300

    Done!

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,44 +1,101 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list/utils/todo_list.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  HomePage({super.key});
 
-  static const List toDoList = [
-    ['Learn Web Development', false],
-    ['Drink Coffe', false],
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _controller = TextEditingController();
+  List toDoList = [
+    ['Code With Otabek', true],
+    ['Learn Flutter', true],
+    ['Drink Coffee', false],
+    ['Explore Firebase', false],
   ];
+
+  void checkBoxChanged(int index) {
+    setState(() {
+      toDoList[index][1] = !toDoList[index][1];
+    });
+  }
+
+  void saveNewTask() {
+    setState(() {
+      toDoList.add([_controller.text, false]);
+      _controller.clear();
+    });
+  }
+
+  void deleteTask(int index) {
+    setState(() {
+      toDoList.removeAt(index);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
       backgroundColor: Colors.deepPurple.shade300,
       appBar: AppBar(
-        title: const Text("Simple Todo"),
+        title: const Text(
+          'Simple Todo',
+        ),
         backgroundColor: Colors.deepPurple,
         foregroundColor: Colors.white,
       ),
       body: ListView.builder(
-          itemCount: toDoList.length,
-          itemBuilder: (BuildContext context, index) {
-            return Padding(
-                padding: const EdgeInsets.only(
-                    top: 20, left: 20, right: 20, bottom: 0),
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Colors.deepPurple,
-                      borderRadius: BorderRadius.circular(15)),
-                  padding: const EdgeInsets.all(20),
-                  child: Text(
-                    toDoList[index][0],
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontFamily: 'Monospace',
-                      fontSize: 18,
+        itemCount: toDoList.length,
+        itemBuilder: (BuildContext context, index) {
+          return TodoList(
+            taskName: toDoList[index][0],
+            taskCompleted: toDoList[index][1],
+            onChanged: (value) => checkBoxChanged(index),
+            deleteFunction: (contex) => deleteTask(index),
+          );
+        },
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                ),
+                child: TextField(
+                  controller: _controller,
+                  decoration: InputDecoration(
+                    hintText: 'Add a new todo items',
+                    filled: true,
+                    fillColor: Colors.deepPurple.shade200,
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        color: Colors.deepPurple,
+                      ),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        color: Colors.deepPurple,
+                      ),
+                      borderRadius: BorderRadius.circular(15),
                     ),
                   ),
-                ));
-          }),
+                ),
+              ),
+            ),
+            FloatingActionButton(
+              onPressed: saveNewTask,
+              child: const Icon(Icons.add),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
