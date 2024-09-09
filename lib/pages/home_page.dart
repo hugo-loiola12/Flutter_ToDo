6dc65d2d59617f61db285e11b1463cca2d0a9eb8
Hugo Loiola <hugoloiola.1@gmail.com>
2024-09-09 15:31:46 -0300

    Small changes

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,15 +3,42 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  static const List toDoList = [
+    ['Learn Web Development', false],
+    ['Drink Coffe', false],
+  ];
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
+      backgroundColor: Colors.deepPurple.shade300,
       appBar: AppBar(
-        title: const Text("Simple TODO"),
+        title: const Text("Simple Todo"),
         backgroundColor: Colors.deepPurple,
         foregroundColor: Colors.white,
       ),
+      body: ListView.builder(
+          itemCount: toDoList.length,
+          itemBuilder: (BuildContext context, index) {
+            return Padding(
+                padding: const EdgeInsets.only(
+                    top: 20, left: 20, right: 20, bottom: 0),
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.deepPurple,
+                      borderRadius: BorderRadius.circular(15)),
+                  padding: const EdgeInsets.all(20),
+                  child: Text(
+                    toDoList[index][0],
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'Monospace',
+                      fontSize: 18,
+                    ),
+                  ),
+                ));
+          }),
     );
   }
 }
